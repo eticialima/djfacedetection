d1568bbbd55f9136b0994cb9a3e05a4d36465c83
eticialima <contato.leticialima.me@gmail.com>
2022-05-01 12:16:33 -0300

    Face detection django

--- recognition/models.py
+++ recognition/models.py
@@ -1,10 +1,10 @@
 from django.db import models
- 
+
 # Create your models here.
 class FaceRecognition(models.Model):
     id = models.AutoField(primary_key=True)
-    record_date = models.DateTimeField(auto_now_add=True) 
-    image = models.ImageField(upload_to='images/')
+    record_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to = 'images/')
 
 
     def __str__(self):
